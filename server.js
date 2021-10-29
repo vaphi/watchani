d31006dc3d3cd817c748df06bc713c7f26df2a7e
Va <phi.va96@gmail.com>
2021-10-28 21:46:50 -0400

    Fixed heruko

--- server.js
+++ server.js
@@ -1,10 +1,3 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
     //Install express server
     const express = require('express');
     const path = require('path');
